ac927dfcdb5d1bc1acad624c38c15e7ec495bcfb
washington barreto <washington.perreiira@hotmail.com>
2022-04-12 09:29:26 -0300

    Adicionar arquivos de projeto.

---
+++ DesktopApp001/FormHome.Designer.cs
@@ -0,0 +1,156 @@
+﻿
+namespace DesktopApp001
+{
+    partial class FormHome
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.tbNomeCompleto = new System.Windows.Forms.TextBox();
+            this.lblNomeCompleto = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.tbEmail = new System.Windows.Forms.TextBox();
+            this.lblSenha = new System.Windows.Forms.Label();
+            this.tbSenha = new System.Windows.Forms.TextBox();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblTitulo.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.lblTitulo.Location = new System.Drawing.Point(230, 48);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(339, 30);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "Cadastro de Pessoas Alegre, 2022";
+            // 
+            // tbNomeCompleto
+            // 
+            this.tbNomeCompleto.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.tbNomeCompleto.Location = new System.Drawing.Point(102, 147);
+            this.tbNomeCompleto.Name = "tbNomeCompleto";
+            this.tbNomeCompleto.Size = new System.Drawing.Size(668, 23);
+            this.tbNomeCompleto.TabIndex = 1;
+            // 
+            // lblNomeCompleto
+            // 
+            this.lblNomeCompleto.AutoSize = true;
+            this.lblNomeCompleto.Font = new System.Drawing.Font("Vineta BT", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblNomeCompleto.Location = new System.Drawing.Point(12, 153);
+            this.lblNomeCompleto.Name = "lblNomeCompleto";
+            this.lblNomeCompleto.Size = new System.Drawing.Size(70, 17);
+            this.lblNomeCompleto.TabIndex = 2;
+            this.lblNomeCompleto.Text = "Nome:";
+            this.lblNomeCompleto.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Font = new System.Drawing.Font("Swis721 Hv BT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblEmail.Location = new System.Drawing.Point(12, 237);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(58, 19);
+            this.lblEmail.TabIndex = 3;
+            this.lblEmail.Text = "email:";
+            // 
+            // tbEmail
+            // 
+            this.tbEmail.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.tbEmail.Location = new System.Drawing.Point(102, 237);
+            this.tbEmail.Name = "tbEmail";
+            this.tbEmail.Size = new System.Drawing.Size(668, 23);
+            this.tbEmail.TabIndex = 5;
+            // 
+            // lblSenha
+            // 
+            this.lblSenha.AutoSize = true;
+            this.lblSenha.Font = new System.Drawing.Font("Stencil", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblSenha.Location = new System.Drawing.Point(12, 330);
+            this.lblSenha.Name = "lblSenha";
+            this.lblSenha.Size = new System.Drawing.Size(65, 19);
+            this.lblSenha.TabIndex = 6;
+            this.lblSenha.Text = "Senha:";
+            // 
+            // tbSenha
+            // 
+            this.tbSenha.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.tbSenha.Location = new System.Drawing.Point(102, 330);
+            this.tbSenha.Name = "tbSenha";
+            this.tbSenha.PasswordChar = '*';
+            this.tbSenha.Size = new System.Drawing.Size(240, 23);
+            this.tbSenha.TabIndex = 7;
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnSalvar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnSalvar.Location = new System.Drawing.Point(709, 395);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(79, 43);
+            this.btnSalvar.TabIndex = 8;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = false;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
+            // 
+            // FormHome
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.tbSenha);
+            this.Controls.Add(this.lblSenha);
+            this.Controls.Add(this.tbEmail);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.lblNomeCompleto);
+            this.Controls.Add(this.tbNomeCompleto);
+            this.Controls.Add(this.lblTitulo);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormHome";
+            this.Text = "Aplicativo de Cadastro";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.TextBox tbNomeCompleto;
+        private System.Windows.Forms.Label lblNomeCompleto;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.TextBox tbEmail;
+        private System.Windows.Forms.Label lblSenha;
+        private System.Windows.Forms.TextBox tbSenha;
+        private System.Windows.Forms.Button btnSalvar;
+    }
+}
+
---
+++ DesktopApp001/FormHome.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace DesktopApp001
+{
+    public partial class FormHome : Form
+    {
+        public FormHome()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Test");
+        }
+    }
+}
